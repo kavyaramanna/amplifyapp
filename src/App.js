f7f596fe783b6eb15c7a0705641fd879b2e9b0df
kavya M R <kavya.mr@ults.in>
2020-12-19 21:59:23 +0530

    add auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          all the best!No quiting
-        </a>
+        <AmplifySignOut />
+        <h2>MY app content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
