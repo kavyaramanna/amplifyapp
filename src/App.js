70d0afbdcc7250dc2c2eb6b4ec9ad7ed57df2f8f
kavya M R <kavya.mr@ults.in>
2020-12-20 10:22:29 +0530

    added changes to auth environment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import Amplify from "aws-amplify";
-// import awsconfig from "./aws-exports";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
 function App() {
   return (
